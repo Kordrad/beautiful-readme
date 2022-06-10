be91e6023e59f70252b4ee1444ad56cff47a6e96
Kordrad <kord.stp@gmail.com>
2022-06-10 14:57:45 +0200

    initial commit

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'beautiful-readme-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
